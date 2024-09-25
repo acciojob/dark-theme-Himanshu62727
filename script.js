08ab40a3d138732277c5c99d8423935fb2c5d4d0
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-09-25 18:47:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
 
+// let btn = document.querySelector("#swap");
+// document.body.style.background = "black"
+// btn.addEventListner("click", ChangeTheme);
+
+
+function changeBackgroundColor() {
+	document.body.style.backgroundColor = "black";
+}
